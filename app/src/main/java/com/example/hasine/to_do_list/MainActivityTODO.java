ea14c38821d268227d985b1768b4407bb9fe5364
Hasine Efeturk <hasineefeturk@hotmail.com>
2015-11-26 15:43:30 +0100

    done

--- app/src/main/java/com/example/hasine/to_do_list/MainActivityTODO.java
+++ app/src/main/java/com/example/hasine/to_do_list/MainActivityTODO.java
@@ -8,38 +8,98 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListAdapter;
+import android.widget.ListView;
+
+import org.apache.commons.io.FileUtils;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.Serializable;
+import java.util.AbstractSequentialList;
+import java.util.ArrayList;
+import java.util.Deque;
+import java.util.List;
 
 public class MainActivityTODO extends AppCompatActivity {
+
+    ListView lv;
+    private ArrayList<String> items;
+    private ArrayAdapter<String> lvAdapter;
+    private Button add;
+    private EditText todoItem;
+    public String addTODOitem;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_activity_todo);
 
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+        lv = (ListView) findViewById(R.id.list);
+        items = new ArrayList<>();
+        readItems();
+        lvAdapter = new ArrayAdapter<String>(this,
+                android.R.layout.simple_list_item_1, items);
+        lv.setAdapter(lvAdapter);
 
+        // Setup remove listener method call
+        setupListViewListener();
+
+
+        // find button and handle action when it is clicked
+        add = (Button) findViewById(R.id.button);
+        add.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // find Edittext
+                todoItem = (EditText) findViewById(R.id.todoItem);
+                addTODOitem = todoItem.getText().toString();
+                lvAdapter.add(addTODOitem);
+                todoItem.setText("");
+                writeItems();
+            }
+        });
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main_activity_todo, menu);
-        return true;
+    private void setupListViewListener() {
+        lv.setOnItemLongClickListener(
+                new AdapterView.OnItemLongClickListener() {
+                    @Override
+                    public boolean onItemLongClick(AdapterView<?> adapter,
+                                                   View item, int pos, long id) {
+                        // Remove the item within array at position
+                        items.remove(pos);
+                        // Refresh the adapter
+                        lvAdapter.notifyDataSetChanged();
+                        writeItems();
+                        // Return true consumes the long click event (marks it handled)
+                        return true;
+                    }
+
+                });
     }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+    private void readItems(){
+        File filesDir = getFilesDir();
+        File todoFile = new File(filesDir, "todo.txt");
+        try{
+            items = new ArrayList<String>(FileUtils.readLines(todoFile));
+        } catch (IOException e){
+            items = new ArrayList<String>();
         }
+    }
 
-        return super.onOptionsItemSelected(item);
+    private void writeItems(){
+        File filesDir = getFilesDir();
+        File todoFile = new File(filesDir, "todo.txt");
+        try{
+            FileUtils.writeLines(todoFile, items);
+        } catch (IOException e){
+            e.printStackTrace();
+        }
     }
 }
